70fd0ac9e72f774582e11aac17bd3fa230fe8df9
tikosaghinadze <teasaghinadze20@gmail.com>
2024-02-29 00:11:38 +0400

    Feat: Created components with specific values and separate CSS files for improved project clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
-import Header from "./components/Header";
+import "./components/Header/Header.css";
+import "./components/Home/Home.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <>
       <main className="main-container">
         <Header />
+        <Home />
       </main>
     </>
   );
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <div className="home-introduce">
+      <div className="wellcome-card"></div>
+      <section className="cofee-collection"></section>
+      <div className="why-choose-us">
+        <article>
+          <h2></h2>
+          <p></p>
+        </article>
+        <div className="company-benefits">
+          <div className="benefit1"></div>
+          <div className="benefit2"></div>
+          <div className="benefit3"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
