0034f13cbdb6ac0583d3bb34860b8ea52f963ccd
tikosaghinadze <teasaghinadze20@gmail.com>
2024-02-29 01:03:04 +0400

    add some styles for home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,19 @@
+import Image from "../../assets/images/home/mobile/image-hero-coffeepress.jpg";
 const Home = () => {
   return (
     <div className="home-introduce">
-      <div className="wellcome-card"></div>
+      <div
+        className="wellcome-card"
+        style={{ backgroundImage: `url(${Image})` }}
+      >
+        <h2 className="greate-cofee">Great coffee made simple.</h2>
+        <p className="start-your-morning">
+          Start your mornings with the world’s best coffees. Try our expertly
+          curated artisan coffees from our best roasters delivered directly to
+          your door, at your schedule.
+        </p>
+        <button className="createyourPlan-btn">Create your plan</button>
+      </div>
       <section className="cofee-collection"></section>
       <div className="why-choose-us">
         <article>
