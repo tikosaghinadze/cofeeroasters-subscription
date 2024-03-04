2bd68fff00b6ff4143c60f4d147bed0cbbf736c2
tikosaghinadze <teasaghinadze20@gmail.com>
2024-03-04 21:46:40 +0400

    feat:for with stem components and style them

--- src/components/home/Step.jsx
+++ src/components/home/Step.jsx
@@ -1,9 +1,15 @@
 const Step = (props) => {
   return (
     <div className="w-full flex flex-col items-center">
-      <h2>{props.step}</h2>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <h2 className="fraunces font-black text-[72px] leading-[72px] text-cream">
+        {props.step}
+      </h2>
+      <h3 className="font-body-font1 text-darkBlue font-black text-[28px] leading-8 mt-6">
+        {props.title}
+      </h3>
+      <p className="text-[15px] font-body-font2  text-darkBlue leading-[25px] font-normal mt-6 text-center md:text-left">
+        {props.description}
+      </p>
     </div>
   );
 };
--- src/components/home/Steps.jsx
+++ src/components/home/Steps.jsx
@@ -3,7 +3,7 @@ import Step from "./Step";
 const Steps = (props) => {
   return (
     <div className="mt-[660px] w-full flex flex-col items-center">
-      <h2 className="font-body-fon1 font-black text-2xl text-gray">
+      <h2 className=" font-black font-body-font1 text-2xl text-gray">
         How it works
       </h2>
       <ul className="list-none flex mb-10 flex-col items-center mt-20 gap-y-14 w-full">
@@ -18,7 +18,7 @@ const Steps = (props) => {
           );
         })}
       </ul>
-      <button className="no-underline fraunces font-black text-lg leading-[25px] text-light px-8 py-4 rounded-md bg-hulk md:mt-11">
+      <button className="no-underline font-body-font1 font-black text-lg leading-[25px] text-light px-8 py-4 rounded-md bg-hulk md:mt-11">
         Create your plan
       </button>
     </div>
