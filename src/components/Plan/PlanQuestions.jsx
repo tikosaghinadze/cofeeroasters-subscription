5d0da2a8ca3c5ba3edae3bd0b0a3a5e8b63c4f62
tikosaghinadze <teasaghinadze20@gmail.com>
2024-03-14 18:42:13 +0400

    Implemented logic to toggle visibility of answers for each question individually

--- src/components/Plan/PlanQuestions.jsx
+++ src/components/Plan/PlanQuestions.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const PlanQuestions = (props) => {
+  const [selectedItem, setSelectedItem] = useState(null);
+  const showAnswersHandle = (item) => {
+    setSelectedItem(item === selectedItem ? null : item);
+  };
   return (
     <div className="mt-[110px] w-full flex flex-col items-center gap-[96px]">
       {props.items.map((item, index) => {
@@ -8,36 +14,41 @@ const PlanQuestions = (props) => {
               <h2 className=" text-gray font-body-font1 text-2xl font-black leading-7">
                 {item.question}
               </h2>
-
-              <img
-                className="w-[18px] h-[11px]"
-                src={item.closeArrow}
-                alt="arrow svg"
-              />
-              <img
-                className="w-[18px] h-[11px]"
-                src={item.openArrow}
-                alt="arrow svg"
-              />
+              {selectedItem === item ? (
+                <img
+                  onClick={() => showAnswersHandle(item)}
+                  className="w-[18px] h-[11px]"
+                  src={item.closeArrow}
+                  alt="arrow svg"
+                />
+              ) : (
+                <img
+                  onClick={() => showAnswersHandle(item)}
+                  className="w-[18px] h-[11px]"
+                  src={item.openArrow}
+                  alt="arrow svg"
+                />
+              )}
             </div>
-
-            <section className="w-full flex flex-col items-center gap-4">
-              {item.options.map((type) => {
-                return (
-                  <div
-                    className="w-full h-[140px] p-6 flex flex-col gap-2 bg-gray2 rounded-lg cursor-pointer"
-                    key={type.id}
-                  >
-                    <h3 className="text-darkBlue text-2xl font-black font-body-font1 leading-8">
-                      {type.type}
-                    </h3>
-                    <p className="text-darkBlue font-body-font2 font-normal text-base leading-[26px]">
-                      {type.answer}
-                    </p>
-                  </div>
-                );
-              })}
-            </section>
+            {selectedItem === item && (
+              <section className="w-full flex flex-col items-center gap-4">
+                {item.options.map((type) => {
+                  return (
+                    <div
+                      className="w-full h-[140px] p-6 flex flex-col gap-2 bg-gray2 rounded-lg cursor-pointer"
+                      key={type.id}
+                    >
+                      <h3 className="text-darkBlue text-2xl font-black font-body-font1 leading-8">
+                        {type.type}
+                      </h3>
+                      <p className="text-darkBlue font-body-font2 font-normal text-base leading-[26px]">
+                        {type.answer}
+                      </p>
+                    </div>
+                  );
+                })}
+              </section>
+            )}
           </div>
         );
       })}
