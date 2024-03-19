d84b9c37a61a482669ac1463679708ca4d6d3ea6
tikosaghinadze <teasaghinadze20@gmail.com>
2024-03-19 19:47:44 +0400

    feat: Enhance PlanQuestions component to display selected answers in ORDER SUMMARY

--- src/components/Plan/PlanQuestions.jsx
+++ src/components/Plan/PlanQuestions.jsx
@@ -2,48 +2,44 @@ import { useState } from "react";
 
 const PlanQuestions = (props) => {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [selectedAnswers, setSelectedAnswers] = useState({});
+
   const showAnswersHandle = (item) => {
     setSelectedItem(item === selectedItem ? null : item);
-    setSelectedAnswer(null);
   };
-  const selectAnswer = (answer) => {
-    setSelectedAnswer(answer);
+
+  const selectAnswer = (itemId, answerId) => {
+    setSelectedAnswers({
+      ...selectedAnswers,
+      [itemId]: answerId,
+    });
   };
+
   return (
     <div className="mt-[110px] w-full flex flex-col items-center gap-[96px]">
       {props.items.map((item, index) => {
         return (
           <div key={item.id} className="w-full flex flex-col justify-start ">
             <div className="mb-8 flex flex-beatween items-center gap-[69px]">
-              <h2 className=" text-gray font-body-font1 text-2xl font-black leading-7">
+              <h2 className="text-gray font-body-font1 text-2xl font-black leading-7">
                 {item.question}
               </h2>
-              {selectedItem === item ? (
-                <img
-                  onClick={() => showAnswersHandle(item)}
-                  className="w-[18px] h-[11px]"
-                  src={item.closeArrow}
-                  alt="arrow svg"
-                />
-              ) : (
-                <img
-                  onClick={() => showAnswersHandle(item)}
-                  className="w-[18px] h-[11px]"
-                  src={item.openArrow}
-                  alt="arrow svg"
-                />
-              )}
+              <img
+                onClick={() => showAnswersHandle(item)}
+                className="w-[18px] h-[11px]"
+                src={selectedItem === item ? item.closeArrow : item.openArrow}
+                alt="arrow svg"
+              />
             </div>
             {selectedItem === item && (
               <section className="w-full flex flex-col items-center gap-4">
                 {item.options.map((type) => {
                   return (
                     <div
-                      onClick={() => selectAnswer(type.id)}
+                      onClick={() => selectAnswer(item.id, type.id)}
                       className={`w-full h-[140px] p-6 flex flex-col gap-2 rounded-lg cursor-pointer ${
-                        selectedAnswer === type.answer
-                          ? "bg-blue text-white"
+                        selectedAnswers[item.id] === type.id
+                          ? "bg-hulk text-white"
                           : "bg-gray2"
                       }`}
                       key={type.id}
@@ -64,18 +60,65 @@ const PlanQuestions = (props) => {
       })}
 
       <div className="w-full px-6 py-[37px] flex flex-col bg-darkBlue rounded-[10px] ">
-        <h3 className=" font-body-font2 text-gray font-black text-[16px] font ">
+        <h3 className="font-body-font2 text-gray font-black text-[16px] font ">
           ORDER SUMMARY
         </h3>
         <p className="mt-[10px] font-body-font1 text-white text-2xl font-black leading-10">
-          “I drink my coffee as <span className=" text-hulk">Filter</span> ,
-          with a <span className=" text-hulk">Decaf</span> type of bean.{" "}
-          <span className=" text-hulk">250g</span> ground ala{" "}
-          <span className=" text-hulk">Cafetiére</span>, sent to me{" "}
-          <span className=" text-hulk">Every Week</span>.”
+          {"I drink my coffee as "}
+          {selectedAnswers[props.items[0].id] && (
+            <span className="text-hulk">
+              {
+                props.items[0].options.find(
+                  (opt) => opt.id === selectedAnswers[props.items[0].id]
+                ).type
+              }
+            </span>
+          )}
+          {", with a "}
+          {selectedAnswers[props.items[1].id] && (
+            <span className="text-hulk">
+              {
+                props.items[1].options.find(
+                  (opt) => opt.id === selectedAnswers[props.items[1].id]
+                ).type
+              }
+            </span>
+          )}
+          {" type of bean. "}
+          {selectedAnswers[props.items[2].id] && (
+            <span className="text-hulk">
+              {
+                props.items[2].options.find(
+                  (opt) => opt.id === selectedAnswers[props.items[2].id]
+                ).type
+              }
+            </span>
+          )}
+          {" ground ala "}
+          {selectedAnswers[props.items[3].id] && (
+            <span className="text-hulk">
+              {
+                props.items[3].options.find(
+                  (opt) => opt.id === selectedAnswers[props.items[3].id]
+                ).type
+              }
+            </span>
+          )}
+          {", sent to me "}
+          {selectedAnswers[props.items[4].id] && (
+            <span className="text-hulk">
+              {
+                props.items[4].options.find(
+                  (opt) => opt.id === selectedAnswers[props.items[4].id]
+                ).type
+              }
+            </span>
+          )}
+          {"."}
         </p>
       </div>
     </div>
   );
 };
+
 export default PlanQuestions;
